7317283a3a46effb677485e1472680ffb31a5bf6
cardacoro <idanieluribe@gmail.com>
2023-02-01 10:02:31 -0500

    Ejercicio Archivo JS

---
+++ archivo.js
@@ -0,0 +1,22 @@
+// - Tu nombre (string)
+var nombre = "Carlos Daniel Coronado Uribe"
+
+// - Tu edad (number)
+var edad = 24
+
+// - ¿Eres desarrollador? (boolean)
+var soyDesarrollador = true
+
+// - Tu fecha de nacimiento (Date)
+var date = new Date( "november 4 1998")
+
+
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+var libroFav = {
+    titulo: "Multiverso",
+    autor: "Leonardo Patrignani",
+    fecha: new Date ("February 26, 2013"),
+    url: "https://www.buscalibre.com.co/libro-multiverso-multiverso-1/"
+}
+console.log(libroFav);
